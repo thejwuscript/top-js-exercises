db55141d9b0fa10642ed5c80a5cd84da8d08f6d2
James Wu <ichangjwu@outlook.com>
2021-10-28 21:36:27 +0900

    completed tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const ftoc = function() {
+const ftoc = function(tempinF) {
 
+ let convertedtoC = (tempinF - 32) * 5/9;
+ return Math.round(convertedtoC * 10) / 10;
+ 
 };
 
-const ctof = function() {
+const ctof = function(tempinC) {
+
+  let convertedtoF = (tempinC * 9/5) + 32;
+  return Math.round(convertedtoF*10) / 10;
 
 };
 
@@ -11,3 +17,7 @@ module.exports = {
   ftoc,
   ctof
 };
+
+// round to one decimal place using Math.round(total * 10) / 10
+// [°F] = [°C] × 9⁄5 + 32
+// [°C] = ([°F] − 32) × 5⁄9
